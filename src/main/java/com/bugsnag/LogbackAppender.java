97dbd76f8569820ed776cd685020478e652e4e2d
Tim Carey-Smith <tim@spork.in>
2014-01-02 02:39:58 +1300

    Correctly recover the original Throwable
    * Add configuration for disabling exception handler configuration

--- src/main/java/com/bugsnag/LogbackAppender.java
+++ src/main/java/com/bugsnag/LogbackAppender.java
@@ -4,14 +4,23 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.classic.spi.StackTraceElementProxy;
-import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.classic.spi.ThrowableProxy;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
 
+//import javax.annotation.Nonnull;
+
+//import static com.google.common.base.Preconditions.checkState;
 import static java.lang.String.format;
 
-public class LogbackAppender extends AppenderBase<ILoggingEvent> {
-  private String apiKey;
+public class LogbackAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
   private Client client;
+
+  private String apiKey;
+
+  private String releaseStage;
+
+  private boolean installHandler = true;
 
   public String getApiKey() {
     return apiKey;
@@ -21,14 +30,43 @@ public class LogbackAppender extends AppenderBase<ILoggingEvent> {
     this.apiKey = apiKey;
   }
 
+  public String getReleaseStage() {
+    return releaseStage;
+  }
+
+  public void setReleaseStage(String releaseStage) {
+    this.releaseStage = releaseStage;
+  }
+
+  public boolean getInstallHandler() {
+    return installHandler;
+  }
+
+  public void setInstallHandler(boolean installHandler) {
+    this.installHandler = installHandler;
+  }
+
   @Override
   public void start() {
+//    checkState(apiKey != null, "apiKey required");
+//    checkState(releaseStage != null, "releaseStage required");
+
     super.start();
-    if (apiKey == null) {
-      throw new IllegalStateException("apiKey required");
+
+    // FIXME: once bugsnag is fixed we don't need this: https://github.com/bugsnag/bugsnag-java/issues/10
+    if (Thread.getDefaultUncaughtExceptionHandler() == null) {
+      Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+          System.err.printf("Exception in thread \"%s\" ", t.getName());
+          e.printStackTrace(System.err);
+        }
+      });
     }
 
-    client = new Client(apiKey);
+    client = new Client(apiKey, installHandler);
+    client.setReleaseStage(releaseStage);
+    client.setUseSSL(true);
   }
 
   @Override
@@ -41,11 +79,29 @@ public class LogbackAppender extends AppenderBase<ILoggingEvent> {
   }
 
   private void notify(Level level, String message, IThrowableProxy throwableProxy) {
-    Throwable throwable = new Throwable(format("%s; %s", message, throwableProxy.getMessage()));
+    MetaData metaData = new MetaData();
+    metaData.addToTab("logging", "level", level.toString());
+    metaData.addToTab("logging", "message", message);
 
-    throwable.setStackTrace(getCallData(throwableProxy));
+    client.notify(rebuildThrowable(throwableProxy), level.toString(), metaData);
+  }
 
-    client.notify(throwable, level.toString());
+  private Throwable rebuildThrowable(/*@Nonnull*/ IThrowableProxy proxy) {
+    if (proxy instanceof ThrowableProxy) {
+      return ((ThrowableProxy) proxy).getThrowable();
+    }
+
+    Throwable cause = null;
+
+    if (proxy.getCause() != null) {
+      cause = rebuildThrowable(proxy.getCause());
+    }
+
+    String message = format("%s: %s", proxy.getClassName(), proxy.getMessage());
+    Throwable throwable = new Throwable(message, cause);
+    throwable.setStackTrace(getCallData(proxy));
+
+    return throwable;
   }
 
   private static StackTraceElement[] getCallData(IThrowableProxy proxy) {
