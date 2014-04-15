28bed22242c06d956bc28f7e947e1f44417b9ed4
Simon Maynard <snmaynard@gmail.com>
2014-04-15 12:07:27 -0700

    Bump version

--- src/main/java/com/bugsnag/Configuration.java
+++ src/main/java/com/bugsnag/Configuration.java
@@ -40,7 +40,7 @@ public class Configuration {
     protected static final String DEFAULT_ENDPOINT = "notify.bugsnag.com";
 
     private static final String NOTIFIER_NAME = "Java Bugsnag Notifier";
-    private static final String NOTIFIER_VERSION = "1.2.3";
+    private static final String NOTIFIER_VERSION = "1.2.4";
     private static final String NOTIFIER_URL = "https://bugsnag.com";
 
     // Notifier settings
