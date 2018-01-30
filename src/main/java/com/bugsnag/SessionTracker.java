1a0f6a3585015440012dacf641ac9f6fe8d94c1d
fractalwrench <fractalwrench@gmail.com>
2018-01-30 16:00:38 +0000

    temp flushing of sessions to test session payload request

--- src/main/java/com/bugsnag/SessionTracker.java
+++ src/main/java/com/bugsnag/SessionTracker.java
@@ -1,28 +1,34 @@
 package com.bugsnag;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 class SessionTracker {
 
-    private final Configuration configuration;
+    private final Configuration config;
     private final ThreadLocal<Session> session = new ThreadLocal<Session>();
-
     private final AtomicLong sessionCount = new AtomicLong();
 
     SessionTracker(Configuration configuration) {
-        this.configuration = configuration;
+        this.config = configuration;
     }
 
     void startNewSession(Date date, boolean autoCaptured) {
-        if ((!configuration.shouldAutoCaptureSessions() && autoCaptured) || !configuration.shouldNotifyForReleaseStage()) {
+        if ((!config.shouldAutoCaptureSessions() && autoCaptured) || !config.shouldNotifyForReleaseStage()) {
             return;
         }
 
         // TODO store date against count and deliver sessions
         sessionCount.incrementAndGet();
         session.set(new Session(UUID.randomUUID().toString(), date));
+
+        // TODO temp flush
+        List<SessionCount> sessionCounts = Collections.singletonList(new SessionCount(session.get().getStartedAtDate(), 1));
+        SessionPayload payload = new SessionPayload(sessionCounts, config);
+        config.sessionDelivery.deliver(config.serializer, payload, config.getErrorApiHeaders());
     }
 
     Session getSession() {
