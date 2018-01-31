beaabbfe325497eb77a37ca08aa899338ba76cd2
fractalwrench <fractalwrench@gmail.com>
2018-01-31 15:19:35 +0000

    add test for flushing session delivery

--- src/main/java/com/bugsnag/SessionTracker.java
+++ src/main/java/com/bugsnag/SessionTracker.java
@@ -1,14 +1,19 @@
 package com.bugsnag;
 
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 class SessionTracker {
 
     private final Configuration config;
     private final ThreadLocal<Session> session = new ThreadLocal<Session>();
+    private final ThreadLocal<Calendar> calendarThreadLocal = new ThreadLocal<Calendar>() {
+        @Override
+        protected Calendar initialValue() {
+            return new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        }
+    };
+    private final Collection<SessionCount> enqueuedSessionCounts = new ConcurrentLinkedQueue<SessionCount>();
 
     SessionTracker(Configuration configuration) {
         this.config = configuration;
@@ -20,20 +25,36 @@ class SessionTracker {
             return;
         }
 
-        // TODO store date against count and deliver sessions
         session.set(new Session(UUID.randomUUID().toString(), date));
+
+
+        // get current minute in utc
+        int minute = getUtcMinute(date);
 
         // TODO temp flush
         SessionCount count = new SessionCount(session.get().getStartedAtDate());
         count.incrementSessionsStarted();
 
-        List<SessionCount> sessionCounts = Collections.singletonList(count);
-        SessionPayload payload = new SessionPayload(sessionCounts, config);
-        config.sessionDelivery.deliver(config.serializer, payload, config.getErrorApiHeaders());
+
     }
 
     Session getSession() {
         return session.get();
+    }
+
+    void flushSessions(Date now) {
+        if (!enqueuedSessionCounts.isEmpty()) {
+            // TODO check empty
+
+            SessionPayload payload = new SessionPayload(enqueuedSessionCounts, config);
+            config.sessionDelivery.deliver(config.serializer, payload, config.getErrorApiHeaders());
+        }
+    }
+
+    private int getUtcMinute(Date date) {
+        Calendar calendar = calendarThreadLocal.get();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MINUTE);
     }
 
 }
--- src/test/java/com/bugsnag/SessionTrackerTest.java
+++ src/test/java/com/bugsnag/SessionTrackerTest.java
@@ -1,9 +1,13 @@
 package com.bugsnag;
 
+import com.bugsnag.delivery.Delivery;
+import com.bugsnag.serialization.Serializer;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
@@ -90,4 +94,95 @@ public class SessionTrackerTest {
         }
     }
 
+    @Test
+    public void zeroSessionDelivery() throws Throwable {
+        CustomDelivery sessionDelivery = new CustomDelivery() {
+            @Override
+            public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+                fail("Should not be called if no sessions enqueued");
+            }
+        };
+        configuration.sessionDelivery = sessionDelivery;
+        sessionTracker.flushSessions(new Date());
+        assertFalse(sessionDelivery.delivered);
+    }
+
+    @Test
+    public void noDateChangeSessionDelivery() throws Throwable {
+        CustomDelivery sessionDelivery = new CustomDelivery() {
+            @Override
+            public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+                fail("Should not be called if date has not exceeded batch period");
+            }
+        };
+        configuration.sessionDelivery = sessionDelivery;
+        sessionTracker.startNewSession(new Date(1309209859), false);
+        sessionTracker.flushSessions(new Date(1309209859));
+        assertTrue(sessionDelivery.delivered);
+    }
+
+    @Test
+    public void multiSessionDelivery() throws Throwable {
+        CustomDelivery sessionDelivery = new CustomDelivery() {
+            @Override
+            public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+                SessionPayload payload = (SessionPayload) object;
+                assertEquals(2, payload.getSessionCounts().size());
+            }
+        };
+        configuration.sessionDelivery = sessionDelivery;
+        sessionTracker.startNewSession(new Date(5092340L), false);
+        sessionTracker.startNewSession(new Date(125098234L), false);
+        sessionTracker.startNewSession(new Date(1509207501L), false);
+        sessionTracker.startNewSession(new Date(1509209834L), false);
+        sessionTracker.flushSessions(new Date(1509209834L));
+        assertTrue(sessionDelivery.delivered);
+    }
+
+    @Test
+    public void sessionDeliveryDiffMin() throws Throwable {
+        CustomDelivery sessionDelivery = new CustomDelivery() {
+            @Override
+            public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+                SessionPayload payload = (SessionPayload) object;
+                assertEquals(1, payload.getSessionCounts().size());
+            }
+        };
+        configuration.sessionDelivery = sessionDelivery;
+
+        // 2 mins apart
+        sessionTracker.startNewSession(new Date(10000000L), false);
+        sessionTracker.flushSessions(new Date(10120000L));
+        assertTrue(sessionDelivery.delivered);
+    }
+
+    @Test
+    public void sessionDeliverySameMin() throws Throwable {
+        CustomDelivery sessionDelivery = new CustomDelivery() {
+            @Override
+            public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+                SessionPayload payload = (SessionPayload) object;
+                assertEquals(1, payload.getSessionCounts().size());
+            }
+        };
+        configuration.sessionDelivery = sessionDelivery;
+
+        // 1 hour apart
+        sessionTracker.startNewSession(new Date(10000000L), false);
+        sessionTracker.flushSessions(new Date(13600000L));
+        assertTrue(sessionDelivery.delivered);
+    }
+
+    abstract static class CustomDelivery implements Delivery {
+        boolean delivered;
+
+        @Override
+        public void deliver(Serializer serializer, Object object, Map<String, String> headers) {
+            delivered = true;
+        }
+
+        @Override
+        public void close() {
+        }
+    }
 }
