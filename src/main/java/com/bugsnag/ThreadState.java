854909301d7a2efa02e0a8a86db18db9ba2a49d6
Delisa <kattrali@users.noreply.github.com>
2018-08-06 15:23:44 -0700

    Revert "Capture trace of error reporting thread and identify with boolean flag" (#88)
    Reverts bugsnag/bugsnag-java#87

--- src/main/java/com/bugsnag/ThreadState.java
+++ src/main/java/com/bugsnag/ThreadState.java
@@ -1,6 +1,5 @@
 package com.bugsnag;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
@@ -10,11 +9,9 @@ import java.util.List;
 import java.util.Map;
 
 class ThreadState {
-
-    private final Configuration config;
-    private final Thread thread;
-    private final StackTraceElement[] stackTraceElements;
-    private Boolean errorReportingThread;
+    private Configuration config;
+    private Thread thread;
+    private StackTraceElement[] stackTraceElements;
 
     ThreadState(Configuration config, Thread thread, StackTraceElement[] stackTraceElements) {
         this.config = config;
@@ -36,16 +33,19 @@ class ThreadState {
         });
 
         List<ThreadState> threads = new ArrayList<ThreadState>();
+        for (int i = 0; i < keys.length; i++) {
+            Thread thread = (Thread) keys[i];
 
-        for (Object key : keys) {
-            Thread thread = (Thread) key;
+            // Don't show the current stacktrace here. It'll point at this method
+            // rather than at the point they crashed.
+            if (thread.getId() == crashingThreadId) {
+                continue;
+            }
+
             ThreadState threadState = new ThreadState(config, thread, liveThreads.get(thread));
             threads.add(threadState);
-
-            if (threadState.getId() == crashingThreadId) {
-                threadState.setErrorReportingThread(true);
-            }
         }
+
         return threads;
     }
 
@@ -62,15 +62,5 @@ class ThreadState {
     @JsonProperty("stacktrace")
     public List<Stackframe> getStacktrace() {
         return Stackframe.getStacktrace(config, stackTraceElements);
-    }
-
-    @JsonProperty("errorReportingThread")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public Boolean isErrorReportingThread() {
-        return errorReportingThread;
-    }
-
-    public void setErrorReportingThread(Boolean errorReportingThread) {
-        this.errorReportingThread = errorReportingThread;
     }
 }
