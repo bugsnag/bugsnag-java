5f4a3247526ff2e95cf853c493c02d7d16bb7b81
Simon Bowring <simon.bowring@bugsnag.com>
2018-09-14 16:31:43 +0100

    Adding documentation/explanation to ExceptionClassCallback

--- bugsnag-spring/src/main/java/com/bugsnag/ExceptionClassCallback.java
+++ bugsnag-spring/src/main/java/com/bugsnag/ExceptionClassCallback.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -25,6 +26,15 @@ import java.util.Map;
 /**
  * A callback to assign a specified severity in a report for particular Spring exceptions.
  * This severity can be overridden if the severity is set manually in an application.
+ *
+ * These exceptions are automatically resolved to HTTP responses by the built-in Spring
+ * {@link DefaultHandlerExceptionResolver}.
+ *
+ * Exceptions that are automatically resolved to 4XX (client error) responses map to
+ * severity INFO because the client is at fault, not the server.
+ *
+ * Exceptions that automatically resolve to 500 (internal server error) responses
+ * map to severity to ERROR.
  */
 class ExceptionClassCallback implements Callback {
 
@@ -32,20 +42,73 @@ class ExceptionClassCallback implements Callback {
 
     static {
         exceptionToSeverity = new HashMap<Class<? extends java.lang.Exception>, Severity>();
-        exceptionToSeverity.put(HttpRequestMethodNotSupportedException.class, Severity.INFO);
-        exceptionToSeverity.put(HttpMediaTypeNotSupportedException.class, Severity.INFO);
-        exceptionToSeverity.put(HttpMediaTypeNotAcceptableException.class, Severity.WARNING);
-        exceptionToSeverity.put(MissingPathVariableException.class, Severity.ERROR);
-        exceptionToSeverity.put(MissingServletRequestParameterException.class, Severity.ERROR);
+
+        // Thrown when a required header, required cookie, etc is missing from the request.
+        // Resolves to HTTP 400, bad request.
         exceptionToSeverity.put(ServletRequestBindingException.class, Severity.INFO);
-        exceptionToSeverity.put(ConversionNotSupportedException.class, Severity.ERROR);
-        exceptionToSeverity.put(TypeMismatchException.class, Severity.INFO);
-        exceptionToSeverity.put(HttpMessageNotReadableException.class, Severity.WARNING);
-        exceptionToSeverity.put(HttpMessageNotWritableException.class, Severity.ERROR);
+
+        // Thrown if an HTTP method is not supported, e.g. a POST request was sent instead
+        // of a GET.
+        // Resolves to HTTP 405, method not allowed.
+        exceptionToSeverity.put(HttpRequestMethodNotSupportedException.class, Severity.INFO);
+
+        // Thrown when a client POSTs, PUTs, or PATCHes content of a type
+        // not supported by request handler.
+        // Resolves to HTTP 415, unsupported media type.
+        exceptionToSeverity.put(HttpMediaTypeNotSupportedException.class, Severity.INFO);
+
+        // Thrown when the request handler cannot generate a response that is acceptable
+        // by the client, expressed by the Accept header.
+        // Resolves to HTTP 406, not acceptable.
+        exceptionToSeverity.put(HttpMediaTypeNotAcceptableException.class, Severity.INFO);
+
+        // Thrown when a method argument annotated with @Valid fails validation.
+        // Resolves to HTTP 400, bad request.
         exceptionToSeverity.put(MethodArgumentNotValidException.class, Severity.INFO);
+
+        // Thrown when part of a multipart request is required but is missing.
+        // Resolves to HTTP 400, bad request.
         exceptionToSeverity.put(MissingServletRequestPartException.class, Severity.INFO);
+
+        // Thrown when a @ModelAttribute method argument has binding or validation errors
+        // and an error response for the client is not defined with a BindingResult.
+        // Resolves to HTTP 400, bad request.
         exceptionToSeverity.put(BindException.class, Severity.INFO);
+
+        // Thrown if a handler cannot be found for the request (404).
+        // Note: By default, the DispatcherServlet does not send exceptions for 404s.  However
+        // if its property "throwExceptionIfNoHandlerFound" is set to true this exception is raised.
+        // Resolves to HTTP 404, not found.
         exceptionToSeverity.put(NoHandlerFoundException.class, Severity.INFO);
+
+        // Thrown when an org.springframework.web.bind.WebDataBinder conversion cannot occur because
+        // a suitable converter could not be found.
+        // Resolves to HTTP 500, internal server error.
+        exceptionToSeverity.put(ConversionNotSupportedException.class, Severity.ERROR);
+
+        // Thrown when an org.springframework.web.bind.WebDataBinder conversion causes an error.
+        // Resolves to HTTP 400, bad request.
+        exceptionToSeverity.put(TypeMismatchException.class, Severity.INFO);
+
+        // Thrown when an HTTP message cannot be read.
+        // Resolves to HTTP 400, bad request.
+        exceptionToSeverity.put(HttpMessageNotReadableException.class, Severity.INFO);
+
+        // Thrown when an HTTP message cannot be written.
+        // Resolves to HTTP 500, internal server error.
+        exceptionToSeverity.put(HttpMessageNotWritableException.class, Severity.ERROR);
+
+        // Thrown if a required method parameter is missing from the request.
+        // Resolves to HTTP 400, bad request.
+        exceptionToSeverity.put(MissingServletRequestParameterException.class, Severity.INFO);
+
+        // Thrown if a controller method's path variable does not match the URI template, e.g.
+        //
+        // @RequestMapping("/example/{variable}")
+        // public String doAThing(@PathVariable String wrongVariable)
+        //
+        // Resolves to HTTP 500, internal server error.
+        exceptionToSeverity.put(MissingPathVariableException.class, Severity.ERROR);
     }
 
     @Override
