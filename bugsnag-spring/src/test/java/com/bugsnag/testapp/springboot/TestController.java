ab416a887ae83e126aa321ce219239e5d45b9165
Simon Bowring <simon.bowring@bugsnag.com>
2018-09-27 16:02:01 +0100

    Change test controller to rest to prevent view-related errors

--- bugsnag-spring/src/test/java/com/bugsnag/testapp/springboot/TestController.java
+++ bugsnag-spring/src/test/java/com/bugsnag/testapp/springboot/TestController.java
@@ -8,12 +8,10 @@ import com.bugsnag.callbacks.Callback;
 
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-
-@Controller
+@RestController
 public class TestController {
 
     @Autowired
