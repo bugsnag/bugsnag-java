289198fdafd4fd1b727a54d779d1859e886259a2
fractalwrench <fractalwrench@gmail.com>
2018-09-28 15:30:02 +0100

    test: verify that Notifier information in payload contains either bugsnag-spring or bugsnag-java

---
+++ bugsnag/src/test/java/com/bugsnag/NotifierTest.java
@@ -0,0 +1,49 @@
+package com.bugsnag;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class NotifierTest {
+
+    private Report report;
+    private Configuration config;
+    private SessionPayload sessionPayload;
+
+    /**
+     * Initialises the objectmapper + report for conversion to json
+     *
+     * @throws Throwable the throwable
+     */
+    @Before
+    public void setUp() throws Throwable {
+        config = new Configuration("api-key");
+        report = new Report(config, new RuntimeException());
+        sessionPayload = new SessionPayload(Collections.<SessionCount>emptyList(), config);
+    }
+
+    @Test
+    public void testNotificationSerialisation() throws Throwable {
+        JsonNode payload = BugsnagTestUtils.mapReportToJson(config, this.report);
+        JsonNode notifier = payload.get("notifier");
+
+        assertEquals("Bugsnag Java", notifier.get("name").asText());
+        assertEquals("https://github.com/bugsnag/bugsnag-java", notifier.get("url").asText());
+        assertNotNull(notifier.get("version").asText());
+    }
+
+    @Test
+    public void testSessionSerialisation() throws Throwable {
+        JsonNode payload = BugsnagTestUtils.mapSessionPayloadToJson(sessionPayload);
+        JsonNode notifier = payload.get("notifier");
+
+        assertEquals("Bugsnag Java", notifier.get("name").asText());
+        assertEquals("https://github.com/bugsnag/bugsnag-java", notifier.get("url").asText());
+        assertNotNull(notifier.get("version").asText());
+    }
+}
