01134e8d627a4497673af691594011af29c5c1c7
fractalwrench <fractalwrench@gmail.com>
2018-10-02 13:45:46 +0100

    style: remove unused import statements

--- bugsnag/src/main/java/com/bugsnag/Notification.java
+++ bugsnag/src/main/java/com/bugsnag/Notification.java
@@ -2,7 +2,6 @@ package com.bugsnag;
 
 import com.bugsnag.serialization.Expose;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
--- bugsnag/src/main/java/com/bugsnag/SessionPayload.java
+++ bugsnag/src/main/java/com/bugsnag/SessionPayload.java
@@ -2,7 +2,6 @@ package com.bugsnag;
 
 import com.bugsnag.serialization.Expose;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
