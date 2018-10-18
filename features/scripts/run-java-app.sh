4ccde42e321c44168e0f4ff57d044c7b225fb631
fractalwrench <fractalwrench@gmail.com>
2018-10-18 12:29:46 +0100

    test: make resources directory before copying logback file

--- features/scripts/run-java-app.sh
+++ features/scripts/run-java-app.sh
@@ -3,7 +3,10 @@
 if [[ -z "${LOGBACK_FILE}" ]]; then
   rm features/fixtures/mazerunner/src/main/resources/logback.xml
 else
-  sed "s#__BUGSNAG_API_KEY__#$BUGSNAG_API_KEY#g; s#__BUGSNAG_MOCK_PATH__#http://localhost:$MOCK_API_PORT#g;" <"features/fixtures/logback/$LOGBACK_FILE" >"features/fixtures/mazerunner/src/main/resources/logback.xml"
+  mkdir features/fixtures/mazerunner/src/main/resources
+  sed "s#__BUGSNAG_API_KEY__#$BUGSNAG_API_KEY#g; s#__BUGSNAG_MOCK_PATH__#http://localhost:$MOCK_API_PORT#g;" \
+   < "features/fixtures/logback/$LOGBACK_FILE" \
+   > "features/fixtures/mazerunner/src/main/resources/logback.xml"
 fi
 
 ./gradlew :features:fixtures:mazerunner:bootRun
