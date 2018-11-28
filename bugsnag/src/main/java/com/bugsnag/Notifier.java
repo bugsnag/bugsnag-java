3bda776a8d2ff5a856fc8a6756b15397c92f5486
fractalwrench <fractalwrench@gmail.com>
2018-11-28 10:46:14 +0000

    build: bump version

--- bugsnag/src/main/java/com/bugsnag/Notifier.java
+++ bugsnag/src/main/java/com/bugsnag/Notifier.java
@@ -5,7 +5,7 @@ import com.bugsnag.serialization.Expose;
 class Notifier {
 
     private static final String NOTIFIER_NAME = "Bugsnag Java";
-    private static final String NOTIFIER_VERSION = "3.4.0";
+    private static final String NOTIFIER_VERSION = "3.4.2";
     private static final String NOTIFIER_URL = "https://github.com/bugsnag/bugsnag-java";
 
     private String notifierName = NOTIFIER_NAME;
