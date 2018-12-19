f3456773930a3fb921937fb30f61e5f6eccd5749
fractalwrench <fractalwrench@gmail.com>
2018-12-19 14:29:42 +0000

    fix: respect different methods of configuring a TaskScheduler
    Alters the ScheduledTaskConfiguration so that it takes into account any TaskScheduler or
    ScheduledExecutorService beans, rather than ovverriding these with a different TaskScheduler. The
    configuration will now use any scheduler on the TaskRegistrar, then any TaskScheduler bean, then any
    ScheduledExecutorService bean, and finally will create its own TaskScheduler. In all cases we
    attempt to set an ErrorHandler on the TaskScheduler so that any unhandled exceptions in scheduled
    tasks are reported.

--- bugsnag-spring/src/main/java/com/bugsnag/ScheduledTaskConfiguration.java
+++ bugsnag-spring/src/main/java/com/bugsnag/ScheduledTaskConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.util.ErrorHandler;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Add configuration for reporting unhandled exceptions for scheduled tasks.
@@ -23,6 +25,9 @@ class ScheduledTaskConfiguration implements SchedulingConfigurer {
     @Autowired
     private Bugsnag bugsnag;
 
+    @Autowired
+    private ScheduledTaskBeanLocator beanLocator;
+
     /**
      * Add bugsnag error handling to a task scheduler
      */
@@ -31,51 +36,82 @@ class ScheduledTaskConfiguration implements SchedulingConfigurer {
         BugsnagScheduledTaskExceptionHandler bugsnagErrorHandler =
                 new BugsnagScheduledTaskExceptionHandler(bugsnag);
 
-        if (taskRegistrar.getScheduler() == null) {
+        // Decision process for finding a TaskScheduler, in order of preference:
+        //
+        // 1. use the scheduler from the task registrar
+        // 2. search for a TaskScheduler bean, by type, then by name
+        // 3. search for a ScheduledExecutorService bean by type, then by name,
+        //    and wrap it in a TaskScheduler
+        // 4. create our own TaskScheduler
 
-            // If no task scheduler has been defined by the application, create one
-            // and add the bugsnag error handler.
-            ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-            taskScheduler.setErrorHandler(bugsnagErrorHandler);
-            taskScheduler.initialize();
-            taskRegistrar.setScheduler(taskScheduler);
+        TaskScheduler taskScheduler = resolveExistingTaskScheduler(taskRegistrar);
+
+        if (taskScheduler != null) {
+            configureExistingTaskScheduler(taskScheduler, bugsnagErrorHandler);
         } else {
-
-            // If a task scheduler has been defined by the application, get it so that
-            // bugsnag error handling can be added.
-            TaskScheduler taskScheduler = taskRegistrar.getScheduler();
-
-            // Reflection is the simplest way to get and set an error handler
-            // because the error handler setter is only defined in the concrete classes,
-            // not the TaskScheduler interface.
-            try {
-                Field errorHandlerField =
-                        taskScheduler.getClass().getDeclaredField("errorHandler");
-                errorHandlerField.setAccessible(true);
-                Object existingErrorHandler = errorHandlerField.get(taskScheduler);
-
-                // If an error handler has already been defined then make the Bugsnag handler
-                // call this afterwards
-                if (existingErrorHandler instanceof ErrorHandler) {
-                    bugsnagErrorHandler.setExistingErrorHandler(
-                            (ErrorHandler) existingErrorHandler);
-                }
-
-                // Add the bugsnag error handler to the scheduler.
-                errorHandlerField.set(taskScheduler, bugsnagErrorHandler);
-            } catch (NoSuchFieldException ex) {
-                logScheduledErrorHandlerNotConfigured();
-            } catch (IllegalArgumentException ex) {
-                logScheduledErrorHandlerNotConfigured();
-            } catch (IllegalAccessException ex) {
-                logScheduledErrorHandlerNotConfigured();
-            } catch (SecurityException ex) {
-                logScheduledErrorHandlerNotConfigured();
-            }
+            ScheduledExecutorService executorService
+                    = beanLocator.resolveScheduledExecutorService();
+            taskScheduler = createNewTaskScheduler(executorService, bugsnagErrorHandler);
+            taskRegistrar.setScheduler(taskScheduler);
         }
     }
 
-    private void logScheduledErrorHandlerNotConfigured() {
-        LOGGER.warn("Bugsnag scheduled task exception handler could not be configured");
+    private TaskScheduler resolveExistingTaskScheduler(ScheduledTaskRegistrar taskRegistrar) {
+        TaskScheduler registrarScheduler = taskRegistrar.getScheduler();
+
+        if (registrarScheduler != null) {
+            return registrarScheduler;
+        } else {
+            return beanLocator.resolveTaskScheduler();
+        }
+    }
+
+    private TaskScheduler createNewTaskScheduler(
+            ScheduledExecutorService executorService,
+            BugsnagScheduledTaskExceptionHandler errorHandler) {
+        if (executorService != null) {
+            // create a task scheduler which delegates to the existing Executor
+            ConcurrentTaskScheduler scheduler = new ConcurrentTaskScheduler(executorService);
+            scheduler.setErrorHandler(errorHandler);
+            return scheduler;
+        } else {
+            // If no task scheduler has been defined by the application, create one
+            // and add the bugsnag error handler.
+            ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+            scheduler.setErrorHandler(errorHandler);
+            scheduler.initialize();
+            return scheduler;
+        }
+    }
+
+    /**
+     * If a task scheduler has been defined by the application, get it so that
+     * bugsnag error handling can be added.
+     * <p>
+     * Reflection is the simplest way to get and set an error handler
+     * because the error handler setter is only defined in the concrete classes,
+     * not the TaskScheduler interface.
+     *
+     * @param taskScheduler the task scheduler
+     */
+    private void configureExistingTaskScheduler(TaskScheduler taskScheduler,
+                                                BugsnagScheduledTaskExceptionHandler errorHandler) {
+        try {
+            Field errorHandlerField =
+                    taskScheduler.getClass().getDeclaredField("errorHandler");
+            errorHandlerField.setAccessible(true);
+            Object existingErrorHandler = errorHandlerField.get(taskScheduler);
+
+            // If an error handler has already been defined then make the Bugsnag handler
+            // call this afterwards
+            if (existingErrorHandler instanceof ErrorHandler) {
+                errorHandler.setExistingErrorHandler((ErrorHandler) existingErrorHandler);
+            }
+
+            // Add the bugsnag error handler to the scheduler.
+            errorHandlerField.set(taskScheduler, errorHandler);
+        } catch (Throwable ex) {
+            LOGGER.warn("Bugsnag scheduled task exception handler could not be configured");
+        }
     }
 }
