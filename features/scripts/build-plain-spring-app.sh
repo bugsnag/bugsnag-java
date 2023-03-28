88ff740117e8d74d8529dded3efd965c9718970d
David Petran <david.petran@SBUK1M97FG4.local>
2023-03-28 17:03:46 +0100

    fix for not able to build bugsnag-spring project on java8 machine

--- features/scripts/build-plain-spring-app.sh
+++ features/scripts/build-plain-spring-app.sh
@@ -7,5 +7,5 @@ if [[ "${JAVA_VERSION}" == "8"* ]]; then
     cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
 else
     ./gradlew -p features/fixtures/mazerunnerplainspring6 war
-    cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring6.war $CATALINA_HOME/webapps/ROOT.war
+    cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
 fi
