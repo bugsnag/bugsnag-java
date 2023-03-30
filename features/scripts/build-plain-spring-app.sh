99fa977cdbbc46da6a41738d38d7d387663a7ff8
David Petran <david.petran@SBUK1M97FG4.local>
2023-03-30 11:01:27 +0100

    fix for war location

--- features/scripts/build-plain-spring-app.sh
+++ features/scripts/build-plain-spring-app.sh
@@ -7,5 +7,5 @@ if [[ "${JAVA_VERSION}" == "8"* ]]; then
     cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
 else
     ./gradlew -p features/fixtures/mazerunnerplainspring6 war
-    cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
+    cp features/fixtures/mazerunnerplainspring6/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
 fi
