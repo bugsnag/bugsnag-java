3e11fade74e3dea55c240b2cf82669f2e2f9a40b
lemnik <lemnik@lemnik.com>
2023-06-27 16:58:47 +0100

    build: publish library to a local repository exported as a BuildKite artifact

--- features/scripts/assemble-fixtures.sh
+++ features/scripts/assemble-fixtures.sh
@@ -2,7 +2,5 @@
 
 if [ ! -d "features/fixtures/libs" ]; then
     mkdir -p features/fixtures/libs
-    ./gradlew bugsnag:assemble bugsnag-spring:assemble bugsnag-spring:assemble -Pversion=9.9.9-test
-    cp bugsnag/build/libs/bugsnag-9.9.9-test.jar features/fixtures/libs/bugsnag-9.9.9-test.jar
-    cp bugsnag-spring/build/libs/bugsnag-spring-9.9.9-test.jar features/fixtures/libs/bugsnag-spring-9.9.9-test.jar
+    unzip maven-repository.zip -d features/fixtures/libs
 fi
