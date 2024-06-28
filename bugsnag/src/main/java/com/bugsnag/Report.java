21f9d22bb59c010c92a661b04023723d9199619b
Christian <chrustuanr15@gmail.com>
2024-06-28 14:04:21 +0100

    fixed checkstyle errors

--- bugsnag/src/main/java/com/bugsnag/Report.java
+++ bugsnag/src/main/java/com/bugsnag/Report.java
@@ -2,8 +2,8 @@ package com.bugsnag;
 
 import com.bugsnag.serialization.Expose;
 
-import com.bugsnag.util.RedactedMap;
 import com.bugsnag.util.FilteredMap;
+import com.bugsnag.util.RedactedMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
