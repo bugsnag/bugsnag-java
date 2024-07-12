415f03e63f22ada46a75bbaca22163c9964d8b92
Christian <chrustuanr15@gmail.com>
2024-07-12 21:13:09 +0100

    scenario syntax fix

--- features/fixtures/scenarios/src/main/java/com/bugsnag/mazerunner/scenarios/ManualRedactedKeysScenario.java
+++ features/fixtures/scenarios/src/main/java/com/bugsnag/mazerunner/scenarios/ManualRedactedKeysScenario.java
@@ -21,8 +21,8 @@ public class ManualRedactedKeysScenario extends Scenario {
         bugsnag.notify(generateException(), new Callback() {
             @Override
             public void beforeNotify(Report report) {
-                report.addToTab("user", "foo", "hunter2");
-                report.addToTab("custom", "abcd", "hunter2");
+                report.addToTab("custom", "foo", "hunter2");
+                report.addToTab("user", "abcd", "hunter2");
                 report.addToTab("custom", "bar", "hunter2");
             }
         });
