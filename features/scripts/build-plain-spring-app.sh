2276c37b5891dc04300184abc14a4321c933125a
richard elms <richardelms@gmail.com>
2025-10-14 22:55:40 +0200

    remove spring boot < v3

--- features/scripts/build-plain-spring-app.sh
+++ features/scripts/build-plain-spring-app.sh
@@ -2,10 +2,5 @@
 
 # Start Tomcat then copy a WAR file across to serve it
 catalina.sh start
-if [[ "${JAVA_VERSION}" == "8"* ]]; then
-    ./gradlew -p features/fixtures/mazerunnerplainspring war
-    cp features/fixtures/mazerunnerplainspring/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
-else
-    ./gradlew -p features/fixtures/mazerunnerplainspring6 war
-    cp features/fixtures/mazerunnerplainspring6/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
-fi
+./gradlew -p features/fixtures/mazerunnerplainspring6 war
+cp features/fixtures/mazerunnerplainspring6/build/libs/mazerunnerplainspring.war $CATALINA_HOME/webapps/ROOT.war
